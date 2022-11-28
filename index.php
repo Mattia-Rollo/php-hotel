97438bab3fc6f5a836538bedad720b7e6d12d43c
Mattia-Rollo-Portatile <ste.tia@hotmail.it>
2022-11-28 14:53:18 +0100

    table hotels

--- index.php
+++ index.php
@@ -39,5 +39,48 @@
         ],
 
     ];
+    
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+    
+   <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">hotel</th>
+      <th scope="col">description</th>
+      <th scope="col">parking</th>
+      <th scope="col">vote</th>
+      <th scope="col">distance</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php foreach($hotels as $hotel){?>
+
+<tr><?php 
+$park = $hotel['parking'] ? 'si' : 'no';
+echo "<td>" . $hotel['name']."</td>";
+echo "<td>". $hotel['description'] . " </td>";
+echo "<td>" . $park . "</td>"; 
+echo "<td>" . $hotel['vote'] . "</td>";
+echo "<td>"  . $hotel['distance_to_center'] . "</td> ";
+
+
+?></tr>
+
+<?php }?>
+    
+  </tbody>
+</table>
+</body>
+</html>
